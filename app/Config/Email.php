8a2cb7b018a329e646ddc742adaf5aea4fbdb49d
mulukd <muluk.dharmayana@gmail.com>
2021-03-27 09:32:01 +0700

    add library myth auth

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
 	/**
 	 * @var string
 	 */
-	public $fromEmail;
+	public $fromEmail = 'uzumaryuki@gmail.com';
 
 	/**
 	 * @var string
 	 */
-	public $fromName;
+	public $fromName = 'WEB AUth SIPOYAN';
 
 	/**
 	 * @var string
